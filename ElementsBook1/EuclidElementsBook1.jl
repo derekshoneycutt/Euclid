bc1116bd891bb0b7abe72ce7262321d30252f5f1
Derek Honeycutt <derekshoneycutt@gmail.com>
2023-03-21 20:38:30 -0700

    Add Definition I.4 and clean up a bit

--- ElementsBook1/EuclidElementsBook1.jl
+++ ElementsBook1/EuclidElementsBook1.jl
@@ -1,21 +1,37 @@
+#
+
+
+# Note on additional axioms:
+#       Although they might not all be "axioms" in a traditional
+#       sense, the idea is that they underly the geometry being explored.
+#       Sometimes, that is purely in a constructive "drawing" sense.
+#       Sometimes, that is because Euclid assumed them.
+#       They are numbered but mixed into the items as they are relevant.
+#       Some of these are created as an exercise less than to be help in Euclid.
+
 
 # ==============================================
 #               Definitions
 # ==============================================
 
-# Definition 1 and its highlighting axiom
+# Definition 1 and its highlighting and moving axioms
 include("Definitions/src/001-Point.jl")
 include("AddedAxioms/src/001-PointHighlight.jl")
+include("AddedAxioms/src/002-PointMove.jl")
 
-# Defintion 2 and its highlighting axiom
+# Defintion 2 and its highlighting and moving axioms
 include("Definitions/src/002-Line.jl")
-include("AddedAxioms/src/002-LineHighlight.jl")
+include("AddedAxioms/src/003-LineHighlight.jl")
+include("AddedAxioms/src/004-LineMove.jl")
 
 # Defintion 3
 include("Definitions/src/003-LineExtremities.jl")
 
 # Definition 4
 include("Definitions/src/004-StraightLine.jl")
+include("AddedAxioms/src/005-LineRotate.jl")
+include("AddedAxioms/src/006-LineReflect.jl")
+include("AddedAxioms/src/007-LineIntersect.jl")
 
 
 
@@ -33,22 +49,6 @@ include("Postulates/src/001-DrawStraightLine.jl")
 # Common Notion 1
 include("CommonNotions/src/001-EqualThings.jl")
 
-
-# ==============================================
-#               Additional Axioms
-# ==============================================
-
-#   Intersection Axioms (e.g. Prop I.1):
-include("AddedAxioms/src/003-LineIntersect.jl")
-
-#   Movement Axioms (e.g. Prop I.4):
-#       Some of these are less necessary for mimicking Euclid
-#       as much as they are interesting for animations.
-#       Animations try to mimick the human proving this with Euclid.
-include("AddedAxioms/src/004-PointMove.jl")
-include("AddedAxioms/src/005-LineMove.jl")
-include("AddedAxioms/src/006-LineRotate.jl")
-include("AddedAxioms/src/007-LineReflect.jl")
 
 
 # ==============================================
