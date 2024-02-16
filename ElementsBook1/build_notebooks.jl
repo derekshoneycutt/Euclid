351459e693e7bc83b107545be8a8d3cacdec4fc6
Derek Honeycutt <derekshoneycutt@gmail.com>
2024-02-15 20:50:19 -0800

    Code Cleanup and use the separated EuclidGLMakie.jl project

--- ElementsBook1/build_notebooks.jl
+++ ElementsBook1/build_notebooks.jl
@@ -7,37 +7,67 @@ try; mkpath("build/obj"); catch; end
 try; mkpath("build/gifs"); catch; end
 println("Build directories prepared.")
 
+build_definitions_names = [
+    "001-Point",
+    "002-Line",
+    "003-LineExtremities",
+    "004-StraightLine",
+    "005-Surface",
+    "006-SurfaceExtremities",
+    "007-PlaneSurface",
+    "008-PlaneAngle",
+    "009-RectilinealAngle",
+    "010a-RightAngle",
+    "010b-Perpendicular",
+    "011-ObtuseAngle",
+    "012-AcuteAngle",
+    "013-Boundary",
+    "014-Figure",
+    "015-Circle",
+    "016-Center",
+    "017-Diameter",
+    "018-Semicircle",
+    "019a-Trilateral",
+    "019b-Quadrilateral",
+    "019c-Multilateral",
+    "020a-Equilateral",
+    "020b-Isosceles",
+    "020c-Scalene",
+    "021a-RightTriangles",
+    "021b-ObtuseTriangle",
+    "021c-AcuteTriangle",
+    "022a-Square",
+    "022b-Oblong",
+    "022c-Rhombus",
+    "022d-Rhomboid",
+    "022e-Trapezia",
+    "023-ParallelLines"
+]
+
+build_definitions_obj = [
+    ("build/obj/Elements1_Def_" * name * ".jl",
+     "./Definitions/" * name * ".ipynb",
+     name)
+    for name in build_definitions_names
+]
+
 # First, export all the notebooks to the build dir
 println("Extracting Jupyter notebooks to julia files...")
 @time begin
     # Definitions...
-    nbexport("build/obj/Elements1_Def_001-Point.jl", "./Definitions/001-Point.ipynb")
-    nbexport("build/obj/Elements1_Def_002-Line.jl", "./Definitions/002-Line.ipynb")
-    nbexport("build/obj/Elements1_Def_003-LineExtremities.jl", "./Definitions/003-LineExtremities.ipynb")
-    nbexport("build/obj/Elements1_Def_004-StraightLine.jl", "./Definitions/004-StraightLine.ipynb")
-    nbexport("build/obj/Elements1_Def_005-Surface.jl", "./Definitions/005-Surface.ipynb")
-    nbexport("build/obj/Elements1_Def_006-SurfaceExtremities.jl", "./Definitions/006-SurfaceExtremities.ipynb")
-    nbexport("build/obj/Elements1_Def_007-PlaneSurface.jl", "./Definitions/007-PlaneSurface.ipynb")
-    nbexport("build/obj/Elements1_Def_008-PlaneAngle.jl", "./Definitions/008-PlaneAngle.ipynb")
-    nbexport("build/obj/Elements1_Def_009-RectilinealAngle.jl", "./Definitions/009-RectilinealAngle.ipynb")
-    nbexport("build/obj/Elements1_Def_010-RightPerpendicular.jl", "./Definitions/010-RightPerpendicular.ipynb")
-    nbexport("build/obj/Elements1_Def_011-ObtuseAngle.jl", "./Definitions/011-ObtuseAngle.ipynb")
-    nbexport("build/obj/Elements1_Def_012-AcuteAngle.jl", "./Definitions/012-AcuteAngle.ipynb")
-    nbexport("build/obj/Elements1_Def_013-Boundary.jl", "./Definitions/013-Boundary.ipynb")
-    nbexport("build/obj/Elements1_Def_014-Figure.jl", "./Definitions/014-Figure.ipynb")
-    nbexport("build/obj/Elements1_Def_015-Circle.jl", "./Definitions/015-Circle.ipynb")
-    nbexport("build/obj/Elements1_Def_016-Center.jl", "./Definitions/016-Center.ipynb")
-    nbexport("build/obj/Elements1_Def_017-Diameter.jl", "./Definitions/017-Diameter.ipynb")
-    nbexport("build/obj/Elements1_Def_018-Semicircle.jl", "./Definitions/018-Semicircle.ipynb")
+    foreach(build_definitions_obj) do obj
+        nbexport(obj[1], obj[2])
+    end
 end
 println("Extracted Jupyter notebooks to julia files.")
 
 
 # We the need to do some setup so all the following scripts run something better
-println("Importing Euclid and overriding Jupypter-specific commands for build-specific alternates...")
+println("Importing Euclid and EuclidGLMakie...")
 @time using Euclid
-using GLMakie
-import Euclid.draw_animation
+@time using EuclidGLMakie
+using GLMakie: record
+import EuclidGLMakie.draw_animation
 function draw_animation(doer::Function, chart::EuclidChartSpace, filename::String, timestamps; framerate=24)
     record(doer, chart.f, "build/" * filename, timestamps; framerate=framerate)
     println("Finished: "*filename)
@@ -46,46 +76,14 @@ function draw_animation(doer::Function, chart::EuclidChartSpace, filename::Strin
     record(doer, chart.f, "build/" * filename, range(0,2π, step=2π/(duration*framerate)); framerate=framerate)
     println("Finished: "*filename)
 end
-println("Euclid imported and patched.")
+println("Euclid imported.")
 
 
 # Render definitions
 println("Rendering Euclid Elements Book I, Definitions...")
-println("Euclid Elements Book I Definition 1 Point...")
-@time include("./build/obj/Elements1_Def_001-Point.jl")
-println("Euclid Elements Book I Definition 2 Line...")
-@time include("./build/obj/Elements1_Def_002-Line.jl")
-println("Euclid Elements Book I Definition 3 Line Extremity...")
-@time include("./build/obj/Elements1_Def_003-LineExtremities.jl")
-println("Euclid Elements Book I Definition 4 Straight Line...")
-@time include("./build/obj/Elements1_Def_004-StraightLine.jl")
-println("Euclid Elements Book I Definition 5 Surface...")
-@time include("./build/obj/Elements1_Def_005-Surface.jl")
-println("Euclid Elements Book I Definition 6 Surface Extremities...")
-@time include("./build/obj/Elements1_Def_006-SurfaceExtremities.jl")
-println("Euclid Elements Book I Definition 7 Plane Surface...")
-@time include("./build/obj/Elements1_Def_007-PlaneSurface.jl")
-println("Euclid Elements Book I Definition 8 Plane Angle...")
-@time include("./build/obj/Elements1_Def_008-PlaneAngle.jl")
-println("Euclid Elements Book I Definition 9 Rectilineal Angle...")
-@time include("./build/obj/Elements1_Def_009-RectilinealAngle.jl")
-println("Euclid Elements Book I Definition 10 Right Angle/Perpendicular...")
-@time include("./build/obj/Elements1_Def_010-RightPerpendicular.jl")
-println("Euclid Elements Book I Definition 11 Obtuse Angle...")
-@time include("./build/obj/Elements1_Def_011-ObtuseAngle.jl")
-println("Euclid Elements Book I Definition 12 Acute Angle...")
-@time include("./build/obj/Elements1_Def_012-AcuteAngle.jl")
-println("Euclid Elements Book I Definition 13 Boundary...")
-@time include("./build/obj/Elements1_Def_013-Boundary.jl")
-println("Euclid Elements Book I Definition 14 Figure...")
-@time include("./build/obj/Elements1_Def_014-Figure.jl")
-println("Euclid Elements Book I Definition 15 Circle...")
-@time include("./build/obj/Elements1_Def_015-Circle.jl")
-println("Euclid Elements Book I Definition 16 Center...")
-@time include("./build/obj/Elements1_Def_016-Center.jl")
-println("Euclid Elements Book I Definition 17 Diameter...")
-@time include("./build/obj/Elements1_Def_017-Diameter.jl")
-println("Euclid Elements Book I Definition 18 Semicircle...")
-@time include("./build/obj/Elements1_Def_018-Semicircle.jl")
+foreach(build_definitions_obj) do obj
+    println("Euclid Elements Book I Definition: " * obj[3])
+    @time include("./" * obj[1])
+end
 println("Euclid Elements Book I, Definitions rendered.")
 cd("../../../")
